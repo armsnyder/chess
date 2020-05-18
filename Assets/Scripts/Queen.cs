19f6d4348747d90c348a6b03d71e2565bcb1860b
Adam Snyder <asnyder@ea.com>
2020-05-17 19:41:29 -0700

    Queen

---
+++ Assets/Scripts/Queen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Queen : Piece
+{
+    protected override string SpriteName { get { return "Queen"; } }
+
+    protected override Vector3Int[] MoveSet
+    {
+        get
+        {
+            return new Vector3Int[] {
+                    new Vector3Int(-1, 0, -1),
+                    new Vector3Int(0, -1, -1),
+                    new Vector3Int(1, 0, -1),
+                    new Vector3Int(0, 1, -1),
+                    new Vector3Int(-1, -1, -1),
+                    new Vector3Int(1, -1, -1),
+                    new Vector3Int(1, 1, -1),
+                    new Vector3Int(-1, 1, -1)
+            };
+        }
+    }
+}
