16df34baaf629c4fc2734440f0e2044ac841fc4f
neal <nakfoury@gmail.com>
2020-05-17 19:42:39 -0700

    Add Knight and Pawn scripts

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Piece
+{
+    protected override string SpriteName { get { return "Knight"; } }
+
+    protected override Vector3Int[] MoveSet
+    {
+        get
+        {
+            return new Vector3Int[] {
+                    new Vector3Int(-1, 2, 1),
+                    new Vector3Int(-1, -2, 1),
+                    new Vector3Int(1, 2, 1),
+                    new Vector3Int(1, -2, 1),
+                    new Vector3Int(2, 1, 1),
+                    new Vector3Int(2, -1, 1),
+                    new Vector3Int(-2, 1, 1),
+                    new Vector3Int(-2, -1, 1)
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    protected override string SpriteName { get { return "King"; } }
+
+    protected override Vector3Int[] MoveSet
+    {
+        get
+        {
+            return new Vector3Int[] {
+                    new Vector3Int(0, 1, 1),
+                    new Vector3Int(0, 2, 2),
+                    new Vector3Int(-1, 1, 3),
+                    new Vector3Int(1, 1, 3)
+            };
+        }
+    }
+}
