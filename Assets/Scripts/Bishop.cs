b4518891a9ee84b4977f622f86cff17059b77bf1
Adam Snyder <asnyder@ea.com>
2020-05-17 19:39:35 -0700

    Bishop

---
+++ Assets/Scripts/Bishop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Bishop : Piece
+{
+    protected override string SpriteName { get { return "Bishop"; } }
+
+    protected override Vector3Int[] MoveSet
+    {
+        get
+        {
+            return new Vector3Int[] {
+                    new Vector3Int(-1, -1, -1),
+                    new Vector3Int(-1, 1, -1),
+                    new Vector3Int(1, 1, -1),
+                    new Vector3Int(1, -1, -1)
+            };
+        }
+    }
+}
